3a92078b3d4d6bedfd1c26bd3eab35a435377bc3
demotomohiro <gpuppur@gmail.com>
2025-05-08 18:10:10 +0900

    adds nimact

---
+++ nimact/nimact.nim
@@ -0,0 +1,14 @@
+import std/[os, osproc]
+
+echo "Running nimact"
+if paramCount() > 0:
+  echo "Arguments:"
+  echo commandLineParams()
+
+  let
+    srcFile = paramStr(1)
+    nimSrcFile = changeFileExt(srcFile, "nim")
+  moveFile srcFile, nimSrcFile
+  quit execCmd("nim c -r " & nimSrcFile)
+else:
+  echo "Nothing to do"
